e3a2075da6ac487db9fc52194ad7b8d1fab2522d
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-05 11:02:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            int[] luckyNumber = { 4, 5, 8, 9, 0 };
+
+            Console.WriteLine(luckyNumber[0]);
+
+            string[] friends = new string[5];
+
+            friends[0] = "Jim";
+            friends[1] = "John";
+            friends[2] = "Peter";
+            Console.WriteLine(friends[2]);
+        }
+
+    }
+}
